3fda3ae464221cc4a6bdc81d0827011ef155c728
xiaolu.wang <xiaolu.wang@garena.com>
2022-12-04 20:51:13 +0800

    first version

---
+++ app.js
@@ -0,0 +1,39 @@
+;(function main() {
+  const iframe = document.querySelector('frame[name="x3"]')
+  iframe.addEventListener('load', onLoad)
+})()
+
+function onLoad() {
+  const iWindow = this.contentWindow
+  const iDocument = iWindow.document
+  const head = iDocument.head || iDocument.getElementsByTagName('head')[0]
+  const styleElement = createStyle()
+  head.appendChild(styleElement)
+  console.log('form#zabba style changed')
+}
+
+function createStyle() {
+  var css = `
+  @media (max-width: 576px) {
+    form#zabba table {
+      display: block;
+      width: 100%;
+    }
+    form#zabba tr {
+      display: flex;
+      flex-wrap: wrap;
+      width: 100%;
+    }
+    form#zabba td {
+      width: auto !important;
+    }
+    form#zabba .auto-style2 {
+      white-space: normal;
+      display: inline-block;
+    }
+  }  
+  `
+  var style = document.createElement('style')
+  style.appendChild(document.createTextNode(css))
+  return style
+}
